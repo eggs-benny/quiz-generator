f92430d229cecf7b33f26fb7adfc633199b715e1
eggs-benny <benedict.smith87@gmail.com>
2023-02-27 18:31:30 +0000

    added correct/incorrect logic to buttons

--- src/MultiChoice/MultiChoice.js
+++ src/MultiChoice/MultiChoice.js
@@ -1,5 +1,12 @@
 import './MultiChoice.css';
+import { useState } from 'react';
 
 export function MultiChoice({ answer, isCorrect }) {
-  return <button className="MultiChoice">  {answer} {isCorrect ? 'true' : 'false'}</button>;
+  const [correctIncorrect, setCorrectIncorrect] = useState(answer)
+
+  const handleButtonClick = () => {
+    isCorrect ? setCorrectIncorrect('Correct!') : setCorrectIncorrect('Incorrect :(')
+  }
+
+  return <button className="MultiChoice" onClick={handleButtonClick}>  {correctIncorrect} </button>;
 }
